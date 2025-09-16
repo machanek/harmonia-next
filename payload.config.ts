c69b4ee73d50fcf3b3c549542f8d34f89427e720
machanek <data.grzegorz@gmail.com>
2025-09-16 21:28:06 +0200

    feat: add PayloadCMS v3 configuration

---
+++ payload.config.ts
@@ -0,0 +1,31 @@
+import { buildConfig } from 'payload'
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { Users } from './collections/Users'
+import { Media } from './collections/Media'
+import { Units } from './collections/Units'
+import { ContactMessages } from './collections/ContactMessages'
+import { SiteSettings } from './collections/SiteSettings'
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    bundler: webpackBundler(),
+  },
+  editor: lexicalEditor({}),
+  collections: [Users, Media, Units, ContactMessages, SiteSettings],
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+  plugins: [],
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI,
+    },
+  }),
+})
