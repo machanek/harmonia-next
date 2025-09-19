d5281d2dd7c12c7ed649215ddc308fa3f789e64b
machanek <data.grzegorz@gmail.com>
2025-09-19 16:31:57 +0200

    fix: add PayloadCMS dependencies to main package.json

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
+import { withPayload } from '@payloadcms/next/withPayload'
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['sharp'],
+  },
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
